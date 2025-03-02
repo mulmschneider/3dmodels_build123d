c328f0c89c39a738e59d38f01af3dba8c00d1bcc
Martin Ulmschneider <7497782+mulmschneider@users.noreply.github.com>
2025-03-02 22:10:22 +0100

    pen_roll_holder: v1 that actually works

--- pen_roll_holder/pen_roll_holder.py
+++ pen_roll_holder/pen_roll_holder.py
@@ -5,22 +5,28 @@ base_length = 300
 base_width = 80
 base_height = 7
 
-roll_length = 37
+roll_length = 36
 roll_width = 33
 roll_height = 45
 roll_spacing = 2
 roll_strength = 3
+
+with BuildPart() as roll:
+    Box(roll_length,roll_width,roll_height,align=(Align.CENTER, Align.CENTER,Align.MIN))
+    fillet(roll.edges().group_by(Axis.Z)[1], radius=15)
+    top_face = roll.faces().sort_by(Axis.Z)[-1]
+    bottom_face = roll.faces().sort_by(Axis.Z)[0]
+    offset(amount=-roll_strength, openings=[top_face, bottom_face])
+    #TODO: chamfer / fillet the top edge, so that pens slide in easier
+
 
 with BuildPart() as holder:
     base = Box(base_length,base_width,base_height)
     fillet(base.edges().filter_by(Axis.Z), radius=5)
     with Locations(holder.faces().sort_by(Axis.Z)[-1]):
         with GridLocations(roll_length+roll_spacing,0,7,1):
-            Box(roll_length,roll_width,roll_height,align=(Align.CENTER, Align.CENTER,Align.MIN))
-            Box(roll_length-roll_strength,roll_width-2,roll_height,align=(Align.CENTER, Align.CENTER,Align.MIN),mode=Mode.SUBTRACT)
-            fillet(holder.edges(Select.LAST).group_by(Axis.Z)[-1], radius=0.9)
+            add(roll)
 
 
-#show_all()
 show(holder)
 export_stl(holder.part, "holder.stl")
